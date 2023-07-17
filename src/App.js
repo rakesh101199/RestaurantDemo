ae922b4212d35ccfdc808324df6468fc1f2801f5
rakesh101199 <rakesh101199@gmail.com>
2023-07-17 16:07:49 +0530

    restaurant

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React ,{ Fragment } from "react";
+import Header from "./Components/Layout/Header";
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React, { Fragment} from "react";
+import classes from './Header.module.css';
+import MealsImage from '../../Assests/Meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+
+
+const Header = props => {
+
+    return (<Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={MealsImage} alt="A table with delicious food"></img>
+        </div>
+    </Fragment>
+    );
+};
+
+
+export default Header;
